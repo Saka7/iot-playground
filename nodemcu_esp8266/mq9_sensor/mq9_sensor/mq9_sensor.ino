70ecf1281530e7ad63d4ec8739ab9b0cd6b182d9
Saka7 <IhorSakaylyuk@gmail.com>
2020-01-26 10:19:13 +0200

    refc(mq9_sensor): fix indentation

--- nodemcu_esp8266/mq9_sensor/mq9_sensor/mq9_sensor.ino
+++ nodemcu_esp8266/mq9_sensor/mq9_sensor/mq9_sensor.ino
@@ -13,82 +13,82 @@ float sensor_value = 0;
 
 // Serial monitor set-up for debugging
 void setup_esp_serial() {
-        Serial.begin(9600);
-        ESPserial.begin(115200);
-        ESPserial.println("AT+IPR=9600");
+		Serial.begin(9600);
+		ESPserial.begin(115200);
+		ESPserial.println("AT+IPR=9600");
 
-        delay(1000);
+		delay(1000);
 
-        ESPserial.end();
-        ESPserial.begin(9600);
-        Serial.println("Serial Ready");
-        ESPserial.println("AT+GMR");
+		ESPserial.end();
+		ESPserial.begin(9600);
+		Serial.println("Serial Ready");
+		ESPserial.println("AT+GMR");
 }
 
 void setup_wifi() {
-        WiFi.begin(SSID, WIFI_PASSWORD);
+		WiFi.begin(SSID, WIFI_PASSWORD);
 
-        while (WiFi.status() != WL_CONNECTED) {
-                delay(1000);
-                Serial.println("WiFi Connecting...");
-        }
+		while (WiFi.status() != WL_CONNECTED) {
+				delay(1000);
+				Serial.println("WiFi Connecting...");
+		}
 }
 
 void setup_pins() {
-        pinMode(LED_PIN_01, OUTPUT);
-        pinMode(SENSOR_PIN_01, INPUT);
+		pinMode(LED_PIN_01, OUTPUT);
+		pinMode(SENSOR_PIN_01, INPUT);
 }
 
 void setup_tls() {
-        client->setFingerprint(FINGERPRINT);
+		client->setFingerprint(FINGERPRINT);
 }
 
 void setup() {
-        setup_esp_serial();
-        setup_pins();
-        setup_wifi();
-        setup_tls();
+		setup_esp_serial();
+		setup_pins();
+		setup_wifi();
+		setup_tls();
 }
 
 float gas_value(float mq9_sensor_value) {
-        return mq9_sensor_value / 1024 * VOLTAGE;
+		return mq9_sensor_value / 1024 * VOLTAGE;
 }
 
 String post_request(String path, String req_body) {
-        if (WiFi.status() == WL_CONNECTED) {
-                http.begin(*client, BASE_URL + path);
+		if (WiFi.status() == WL_CONNECTED) {
+				http.begin(*client, BASE_URL + path);
 
-                http.addHeader("content-type", "application/json");
-                http.addHeader("x-api-key", X_API_KEY);
+				http.addHeader("content-type", "application/json");
+				http.addHeader("x-api-key", X_API_KEY);
 
-                int httpCode = http.POST(req_body);
+				int httpCode = http.POST(req_body);
 
-                String payload = httpCode > 0 ? http.getString() : "ERROR";
+				String payload = httpCode > 0 ? http.getString() : "ERROR";
 
-                http.end();
+				http.end();
 
-                return payload;
-        }
+				return payload;
+		}
 }
 
 void loop() {
-        sensor_value = analogRead(SENSOR_PIN_01);
+		sensor_value = analogRead(SENSOR_PIN_01);
 
-        float v = gas_value(sensor_value);
+		float v = gas_value(sensor_value);
 
 		Serial.println(v);
 
-        if (v >= MIN_ALARMING_GAS_VALUE) {
+		if (v >= MIN_ALARMING_GAS_VALUE) {
 				digitalWrite(LED_PIN_01, HIGH);
-                const String body = "{\"v\": " + String(v) + "}";
-                const String result = post_request("/", body);
+				const String body = "{\"v\": " + String(v) + "}";
+				const String result = post_request("/", body);
 
-                Serial.println(result);
+				Serial.println(result);
 
-                delay(HTTP_REQUEST_DELAY);
-        } else {
-			digitalWrite(LED_PIN_01, LOW);
+				delay(HTTP_REQUEST_DELAY);
+		} else {
+				digitalWrite(LED_PIN_01, LOW);
 		}
 
-        delay(SENSOR_DELAY);
+		delay(SENSOR_DELAY);
 }
